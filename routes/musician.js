adaf2832d1e3d77bf96842e53a47f3cf1a003e5f
Evgenii <abellindsey@nwe.group>
2019-10-01 20:43:46 +0300

    qwertyu

--- routes/musician.js
+++ routes/musician.js
@@ -5,9 +5,9 @@ const jsonParser = bodyParser.json();
 const schema = require('../store/schema');
 
 // healthcheck
-router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
-});
+//router.get('/health', (req, res) => {
+  //res.status('200').send("Status: ok!");
+//});
 
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
